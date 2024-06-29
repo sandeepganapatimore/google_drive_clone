b0159fb4c4677a6b06c234be1aebf925e679085f
sandeepganapatimore <moresandeep919@gmail.com>
2024-06-29 12:28:07 +0530

    static button ,icons are implemented

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AppsIcon from '@mui/icons-material/Apps';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+function Header() {
+    return (
+        <div class="header" >
+            <div className="header_logo">
+                <img src="//ssl.gstatic.com/images/branding/product/1x/drive_2020q4_48dp.png" />
+                <span>Drive</span>
+            </div>
+            <div className="header_search">
+                <SearchIcon />
+                <input type='text' placeholder='Search in Drive' />
+                <FormatAlignCenterIcon />
+            </div>
+            <div className="header_icons">
+                <span>
+                    <HelpOutlineIcon />
+                    <SettingsIcon sx={{ ml: 2 }} />
+                </span>
+                <span>
+                    <AppsIcon />
+                    <AccountCircleIcon sx={{ ml: 2 }} />
+                </span>
+            </div>
+        </div>
+    )
+}
+export default Header
